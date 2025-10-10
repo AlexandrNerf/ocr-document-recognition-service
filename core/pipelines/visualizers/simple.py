149c9134739aa60cfe4a6029d561db840f8b3d80
Alex Nerf <a.varnakin@g.nsu.ru>
2025-10-10 18:18:06 +0700

    [CORE-3] Добавить линтеры в ядро

--- core/pipelines/visualizers/simple.py
+++ core/pipelines/visualizers/simple.py
@@ -1,29 +1,31 @@
-import cv2
 import numpy as np
-import plotly.io as pio
-import tempfile
-import webbrowser
 import panel as pn
-from rich.console import Console
 from data.data_classes import Prediction
-from plotly.subplots import make_subplots
 from pipelines.default.visualizer import Visualizer
 from utils.BBoxVisualizer import BoundingBoxVisualizer
 
-console = Console()
+pn.extension("plotly")
 
-class SimpleVisualizer(Visualizer):
+
+class SimpleVisualizer(Visualizer):  # noqa: WPS338
     def _init(self):
         self.figs: list = []
         self.max_width: int | None = 1000
-        pn.extension('plotly')
 
-    def update_figures(self, images: list[np.array], predictions: list[list[Prediction]]):
-        self.max_width = min(self.max_width, max(image.shape[1] for image in images))
+    def update_figures(
+        self, images: list[np.array], predictions: list[list[Prediction]]
+    ):
+        self.max_width = min(
+            self.max_width, max(image.shape[1] for image in images)
+        )  # noqa: WPS221, E501
         for image, prediction in zip(images, predictions):
-            self.figs.append(BoundingBoxVisualizer.show_image(image, prediction, self.max_width))
+            self.figs.append(
+                BoundingBoxVisualizer.show_image(image, prediction, self.max_width)
+            )
 
-    def visualize(self, images: list[np.array], predictions: list[list[Prediction]]) -> None:
+    def visualize(
+        self, images: list[np.array], predictions: list[list[Prediction]]
+    ) -> None:
         """Вывод изображений с использованием BoundingBoxVisualizer
         Args:
             images (list[np.array]): Список изображений
@@ -32,19 +34,13 @@ class SimpleVisualizer(Visualizer):
         Returns:
             out (None): Выведенные изображения
         """
-        print(images)
-        print(predictions)
         self.update_figures(images, predictions)
 
         if len(self.figs) < 1:
-            return Exception('Not founded images to visualize')
-            
+            return Exception("Not founded images to visualize")
+
         panel = pn.Column()
         for fig in self.figs:
             panel.append(pn.pane.Plotly(fig, config={"responsive": True}))
-        
-        panel.show()
 
-        
-        
-            
+        panel.show()
--- core/utils/read_detection.py
+++ core/utils/read_detection.py
@@ -1,28 +1,29 @@
-import ast
 import os
 from typing import List, Tuple, Union
 
 import cv2
 import numpy as np
-from PIL import Image, ImageFile
-
 from data.data_classes import Prediction
+from PIL import Image
 
 
 def read_detections(
     images: List[np.ndarray] | List[Image.Image] | List[str] | str,
     txt_paths: str | List[str],
-    box_format='pascal_voc',
+    box_format="pascal_voc",
     labels_from_file=True,
 ) -> Tuple[List[np.ndarray], List[List[Prediction]]]:
     """
     Чтение детекций из изображений
 
     Args:
-        images (List[np.ndarray] | List[Image.Image] | str | List[str]): Список изображений или путь к папке с ними
-        txt_paths (List[str] | str): Путь до детекций (формат txt) или список детекций при labels_from_file=False
+        images (List[np.ndarray] | List[Image.Image] | str | List[str]):
+        Список изображений или путь к папке с ними
+        txt_paths (List[str] | str):
+        Путь до детекций (формат txt) или список детекций при labels_from_file=False
         box_format (str): Формат ('yolo' или 'pascal_voc')
-        labels_from_file (bool): Если True, то txt_paths должен быть путь до папки с txt файлами, иначе txt_paths должен быть список детекций
+        labels_from_file (bool): Если True, то txt_paths должен быть путь до папки
+                        с txt файлами, иначе txt_paths должен быть список детекций
     """
     detections = []
     imgs = []
@@ -47,7 +48,7 @@ def read_detections(
                 width, height = image.size
 
         if labels_from_file:
-            with open(txt_path, 'r', encoding='utf-8') as f:
+            with open(txt_path, "r", encoding="utf-8") as f:
                 lines = [line.strip() for line in f if line.strip()]
         else:
             lines = txt_paths
@@ -55,9 +56,11 @@ def read_detections(
         image_detections = []
 
         for line in lines:
-            try:
+            try:  # noqa: WPS229
                 text, coords_str = line.split(" ", 1)
-                x_cn, y_cn, w_n, h_n = list(map(float, coords_str.split(' ')))
+                x_cn, y_cn, w_n, h_n = list(
+                    map(float, coords_str.split(" "))
+                )  # noqa: WPS221
 
                 tl_x, tl_y, br_x, br_y = (
                     (x_cn - w_n / 2) * width,
@@ -89,7 +92,7 @@ def read_detections(
                         text_score=1.0,
                     )
                 )
-            except ValueError as _:
+            except ValueError:
                 continue
 
         detections.append(image_detections)
@@ -97,22 +100,24 @@ def read_detections(
     return imgs, detections
 
 
-def crop_masked_rectangle(
+def crop_masked_rectangle(  # noqa: WPS210, WPS234
     image: Union[np.ndarray, Image.Image, str],
-    box: Union[List[tuple[int, int]], List[tuple[float, float]]],
+    box: Union[List[tuple[int, int]], List[tuple[float, float]]],  # noqa: WPS221
 ) -> np.ndarray | Image.Image:
     """
     Вырезает прямоугольную область и вращает изображение (постпроцессинг).
 
     Args:
-        image (np.ndarray | Image.Image | str): Входное изображение в формате BGR или PIL Image, размер (H, W, 3), или путь к файлу.
-        box (list[tuple[int, int]] | list[tuple[float, float]]): Координаты четырёхугольника [(x1, y1), (x2, y2), (x3, y3), (x4, y4)].
-
+        image (np.ndarray | Image.Image | str):
+        Входное изображение в формате BGR или PIL Image,
+        размер (H, W, 3), или путь к файлу.
+        box (list[tuple[int, int]] | list[tuple[float, float]]):
+        Координаты четырёхугольника [(x1, y1), (x2, y2), (x3, y3), (x4, y4)].
     Returns:
-        out (np.ndarray | Image.Image): Вырезанное изображение в формате BGR, размер (h, w, 3),
+        out (np.ndarray | Image.Image): Вырезанное изображение в формате BGR,
+                                        размер (h, w, 3),
         где h и w — размеры ограничивающего прямоугольника.
     """
-
     if isinstance(image, Image.Image):
         img = np.asarray(image)
     elif isinstance(image, str):
@@ -122,26 +127,37 @@ def crop_masked_rectangle(
 
     bbox = np.array(box, dtype=np.float32).reshape(4, 2)
 
-    s = bbox.sum(axis=1)  # x + y
+    summary = bbox.sum(axis=1)  # x + y
     diff = np.diff(box, axis=1)  # x - y
 
     # Определяем углы
-    top_left = bbox[np.argmin(s)]
-    bottom_right = bbox[np.argmax(s)]
+    top_left = bbox[np.argmin(summary)]
+    bottom_right = bbox[np.argmax(summary)]
     top_right = bbox[np.argmin(diff)]
     bottom_left = bbox[np.argmax(diff)]
 
     bbox = np.array([top_left, top_right, bottom_right, bottom_left], dtype=np.float32)
     # Определяем размеры результирующего прямоугольника
     width = int(
-        max(np.linalg.norm(bbox[0] - bbox[1]), np.linalg.norm(bbox[2] - bbox[3]))
+        max(
+            np.linalg.norm(bbox[0] - bbox[1]),
+            np.linalg.norm(bbox[2] - bbox[3]),  # noqa: WPS221
+        )
     )
     height = int(
-        max(np.linalg.norm(bbox[0] - bbox[3]), np.linalg.norm(bbox[1] - bbox[2]))
+        max(
+            np.linalg.norm(bbox[0] - bbox[3]),
+            np.linalg.norm(bbox[1] - bbox[2]),  # noqa: WPS221
+        )
     )
 
     dst = np.array(
-        [[0, 0], [width - 1, 0], [width - 1, height - 1], [0, height - 1]],
+        [
+            [0, 0],
+            [width - 1, 0],
+            [width - 1, height - 1],
+            [0, height - 1],
+        ],  # noqa: WPS221
         dtype=np.float32,
     ).reshape(4, 2)
 
@@ -151,4 +167,4 @@ def crop_masked_rectangle(
     perspective = cv2.getPerspectiveTransform(bbox, dst)
     warped = cv2.warpPerspective(img, perspective, (width, height))
 
-    return warped if type(image) == np.ndarray else Image.fromarray(warped)
+    return warped if image is np.ndarray else Image.fromarray(warped)
